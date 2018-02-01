8c0148b6008203b2af21f0edf1ccfac826cc7c7a
Areeb Sabzposh <areeb.sabzposh@gmail.com>
2018-01-31 23:31:43 -0500

    Solution.

--- StackOverFlow-48554026/ViewController.swift
+++ StackOverFlow-48554026/ViewController.swift
@@ -13,11 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let item = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(buttonPressed))
+        self.navigationItem.rightBarButtonItem = item
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func buttonPressed() {
+        let url = NSURL.init(string: "http://www.probe-lab.com")
+        let items:[Any] = [url!]
+        let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        activityViewController.excludedActivityTypes = [.print,
+                                                        .assignToContact,.saveToCameraRoll,.addToReadingList,.openInIBooks]
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.window?.rootViewController?.present(activityViewController, animated: true, completion: nil)
     }
 
 
